c925d8ac162cf9c5a40f5592d0613a57a72feadf
Ben Elsworth <be15516@bristol.ac.uk>
2017-04-04 11:31:39 +0100

    json parsing

--- pubmed_xml_parser.py
+++ pubmed_xml_parser.py
@@ -19,12 +19,49 @@ def xml_to_json():
 
 def parse_json():
 	for json_file in os.listdir(dataDir+'json/'):
+		counter=0
+		print '#### Parsing '+json_file+' ####'
 		with open(dataDir+'/json/'+json_file) as data_file:
 			json_data = json.load(data_file)
-		if 'publication-base_uk:dois' in json_data['publication-template:GetPublicationResponse']['core:result']['core:content']:
-			print json_data['publication-template:GetPublicationResponse']['core:result']['core:content']['publication-base_uk:dois']
-		else:
-			print json_file+ ".json has no DOI"
+		mData = []
+		#for each publication
+		for p in json_data['PubmedArticleSet']['PubmedArticle']:
+			counter+=1
+			#create empty dictionary
+			#d={'pmid':'','date_created':''}
+			d = {}
+			#parse medline data
+			if 'MedlineCitation' in p:
+				#check for single article files
+				if p == 'MedlineCitation':
+					medLineData = json_data['PubmedArticleSet']['PubmedArticle']['MedlineCitation']
+				else:
+					medLineData = p['MedlineCitation']
+				#pubmed ID
+				if 'PMID' in medLineData:
+					print medLineData['PMID']['#text']
+					d['pmid']=medLineData['PMID']['#text']
+				else:
+					print json_file+' : '+str(counter)+ " has no pmid"
+				#date created
+				if 'DateCreated' in medLineData:
+					d['DateCreated']=medLineData['DateCreated']
+				else:
+					print json_file+' : '+str(counter)+ " has no DateRevised"
+				#date revised
+				if 'DateRevised' in medLineData:
+					d['DateRevised']=medLineData['DateRevised']
+				else:
+					print json_file+' : '+str(counter)+ " has no DateRevised"
+				#article
+				if 'Article' in medLineData:
+					d['Article']=medLineData['Article']
+				else:
+					print json_file+' : '+str(counter)+ " has no Article"
+			else:
+				print str(counter)+" has no MedlineCitation"
+		mData.append(d)
+	print json.dumps(mData)
 
 def main():
 	xml_to_json()
