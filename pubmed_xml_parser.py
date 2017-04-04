59a1cc3c885429750b5ba4b51c83b651b11e82bd
Ben Elsworth <be15516@bristol.ac.uk>
2017-04-04 16:31:20 +0100

    mesh working

--- pubmed_xml_parser.py
+++ pubmed_xml_parser.py
@@ -20,48 +20,104 @@ def xml_to_json():
 def parse_json():
 	for json_file in os.listdir(dataDir+'json/'):
 		counter=0
-		print '#### Parsing '+json_file+' ####'
-		with open(dataDir+'/json/'+json_file) as data_file:
-			json_data = json.load(data_file)
-		mData = []
-		#for each publication
-		for p in json_data['PubmedArticleSet']['PubmedArticle']:
-			counter+=1
-			#create empty dictionary
-			#d={'pmid':'','date_created':''}
+		if os.path.exists(dataDir+'/elastic-json/'+json_file):
+			print 'Elastic JSON file '+json_file+' already created'
+		else:
+			print '#### Parsing '+json_file+' ####'
+			with open(dataDir+'/json/'+json_file) as data_file:
+				json_data = json.load(data_file)
+			mData = []
+			#for each publication
 			d = {}
-			#parse medline data
-			if 'MedlineCitation' in p:
-				#check for single article files
-				if p == 'MedlineCitation':
-					medLineData = json_data['PubmedArticleSet']['PubmedArticle']['MedlineCitation']
+			for p in json_data['PubmedArticleSet']['PubmedArticle']:
+				counter+=1
+				#parse medline data
+				if 'MedlineCitation' in p:
+					#check for single article files
+					if p == 'MedlineCitation':
+						medLineData = json_data['PubmedArticleSet']['PubmedArticle']['MedlineCitation']
+					else:
+						medLineData = p['MedlineCitation']
+					#pubmed ID
+					if 'PMID' in medLineData:
+						print medLineData['PMID']['#text']
+						d['pmid']=medLineData['PMID']['#text']
+					else:
+						print json_file+' : '+str(counter)+ " has no pmid"
+					#date created
+					if 'DateCreated' in medLineData:
+						d['DateCreated']=medLineData['DateCreated']['Year']+'-'+medLineData['DateCreated']['Month']+'-'+medLineData['DateCreated']['Day']
+					else:
+						print json_file+' : '+str(counter)+ " has no DateRevised"
+					#date revised
+					if 'DateRevised' in medLineData:
+						d['DateRevised']=medLineData['DateRevised']['Year']+'-'+medLineData['DateRevised']['Month']+'-'+medLineData['DateRevised']['Day']
+					else:
+						print json_file+' : '+str(counter)+ " has no DateRevised"
+					#article
+					if 'Article' in medLineData:
+						d['ArticleTitle']=medLineData['Article']['ArticleTitle']
+					else:
+						print json_file+' : '+str(counter)+ " has no Article"
+					#mesh
+					if 'MeshHeadingList' in medLineData:
+						meshList = []
+						meshData = medLineData['MeshHeadingList']['MeshHeading']
+						if type(meshData) is list:
+							for mesh in meshData:
+								descriptor = mesh['DescriptorName']['#text']
+								if mesh['DescriptorName']['@MajorTopicYN'] == 'Y':
+									print mesh
+									meshList.append(mesh['DescriptorName']['#text'])
+								if 'QualifierName' in mesh:
+									if type(mesh['QualifierName']) is list:
+										for q in mesh['QualifierName']:
+											if q['@MajorTopicYN'] == 'Y':
+												#print mesh
+												print descriptor+'/'+q['#text']
+												meshList.append(descriptor+'/'+q['#text'])
+									elif type(mesh['QualifierName']) is dict:
+										if mesh['QualifierName']['@MajorTopicYN'] == 'Y':
+											#print mesh
+											print descriptor+'/'+mesh['QualifierName']['#text']
+											meshList.append(descriptor+'/'+mesh['QualifierName']['#text'])
+
+						elif type(meshData) is dict:
+							descriptor = meshData['DescriptorName']['#text']
+							if meshData['DescriptorName']['@MajorTopicYN'] == 'Y':
+								print mesh
+								meshList.append(meshData['DescriptorName']['#text'])
+							if 'QualifierName' in mesh:
+								if type(mesh['QualifierName']) is list:
+									for q in mesh['QualifierName']:
+										if q['@MajorTopicYN'] == 'Y':
+											#print mesh
+											print descriptor+'/'+q['#text']
+											meshList.append(descriptor+'/'+q['#text'])
+								elif type(mesh['QualifierName']) is dict:
+									if mesh['QualifierName']['@MajorTopicYN'] == 'Y':
+										#print mesh
+										print descriptor+'/'+mesh['QualifierName']['#text']
+										meshList.append(descriptor+'/'+mesh['QualifierName']['#text'])
+						d['Mesh'] = meshList
+					#	d['mesh'] = medLineData['MeshHeadingList']['MeshHeading']
 				else:
-					medLineData = p['MedlineCitation']
-				#pubmed ID
-				if 'PMID' in medLineData:
-					print medLineData['PMID']['#text']
-					d['pmid']=medLineData['PMID']['#text']
-				else:
-					print json_file+' : '+str(counter)+ " has no pmid"
-				#date created
-				if 'DateCreated' in medLineData:
-					d['DateCreated']=medLineData['DateCreated']
-				else:
-					print json_file+' : '+str(counter)+ " has no DateRevised"
-				#date revised
-				if 'DateRevised' in medLineData:
-					d['DateRevised']=medLineData['DateRevised']
-				else:
-					print json_file+' : '+str(counter)+ " has no DateRevised"
-				#article
-				if 'Article' in medLineData:
-					d['Article']=medLineData['Article']
-				else:
-					print json_file+' : '+str(counter)+ " has no Article"
-			else:
-				print str(counter)+" has no MedlineCitation"
-		mData.append(d)
-	print json.dumps(mData)
+					print str(counter)+" has no MedlineCitation"
+				#create new data set for new pubmed article
+				if len(p)==2:
+					mData.append(d)
+					d = {}
+			mData.append(d)
+			w = open(dataDir+'/elastic-json/'+json_file,'w')
+			#
+			counter=0
+			for m in mData:
+				if len(m)>1:
+					counter+=1
+					print m
+					w.write('{ "index" : { "_index" : "pubmed-json", "_type" : "type1", "_id" : "'+str(counter)+'" } }\n')
+					w.write(json.dumps(m)+'\n')
+		#print json.dumps(mData)
 
 def main():
 	xml_to_json()
